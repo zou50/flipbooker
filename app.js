6f74e7ca7e7306ec8f5df3ce63ffc8054f3cb5f8
Danny Zou <zou50@purdue.edu>
2020-01-04 12:04:56 -0500

    Attach event listeners to canvas

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-console.log("Flipbooker");
+// Primary app
+var canvas;
+var ctx;
+
+window.onload = function() {
+  console.log("Flipbooker");
+
+  loadCanvas();
+}
+
+function loadCanvas() {
+  canvas = document.getElementById('main-board');
+  ctx = canvas.getContext('2d');
+
+  // Add event listeners
+  canvas.addEventListener('mouseenter', onMouseEnter);
+  canvas.addEventListener('mouseout', onMouseOut);
+  canvas.addEventListener('mousemove', onMouseMove);
+  canvas.addEventListener('mousedown', onMouseDown);
+  canvas.addEventListener('mouseup', onMouseUp);
+}
+
+function onMouseEnter() {
+  console.log("mouse enter");
+}
+
+function onMouseOut() {
+  console.log("mouse out");
+}
+
+function onMouseMove() {
+  console.log("mouse move");
+}
+
+function onMouseDown() {
+  console.log("mouse down");
+}
+
+function onMouseUp() {
+  console.log("mouse up");
+}
